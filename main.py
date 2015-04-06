5029c04e51794b3a15237523bc3ab92aae4784bf
ProgramFOX <programfox@hotmail.be>
2015-04-06 15:10:16 +0200

    Add !>addowner and !>amiowner

--- main.py
+++ main.py
@@ -34,17 +34,45 @@ action_queue = Queue.Queue()
 def on_event(event, _):
     if not isinstance(event, MessagePosted):
         return
-    if event.message.content_source.startswith(prefix + "apiquota"):
+    msg = event.message.content_source
+    if msg.startswith(prefix + "apiquota"):
         event.message.reply(str(fetcher.api_quota))
-    elif event.message.content_source.startswith(prefix + "stop")\
+    elif msg.startswith(prefix + "stop")\
             and event.user.id in owners[host]:
         room.leave()
         c.logout()
         print("Exiting...")
         fetcher.stop()
-    elif event.message.content_source.startswith(prefix + "forcecheck")\
+    elif msg.startswith(prefix + "forcecheck")\
             and event.user.id in owners[host]:
         fetcher.force_check()
+    elif msg.startswith(prefix + "addowner")\
+            and event.user.id in owners[host]:
+        args = msg.split(" ")[1:]
+        new_owner_host = args[0]
+        new_owner_id_s = args[1]
+        if not new_owner_host.endswith(".com"):
+            new_owner_host += ".com"
+        if new_owner_host not in ["stackexchange.com", "stackoverflow.com",
+                                  "meta.stackexchange.com"]:
+            event.message.reply("Invalid host for new owner. "
+                                "Syntax: `!>addowner host id`")
+            return
+        try:
+            new_owner_id = int(new_owner_id_s)
+        except ValueError:
+            event.message.reply("Invalid ID for new owner. "
+                                "Syntax: `!>addowner host id`")
+            return
+        if new_owner_host not in owners:
+            owners[new_owner_host] = []
+        owners[new_owner_host].append(new_owner_id)
+        event.message.reply("New owner added")
+    elif msg.startswith(prefix + "amiowner"):
+        if event.user.id in owners[host]:
+            event.message.reply("Yes, you are an owner.")
+        else:
+            event.message.reply("No, you are not an owner.")
 
 room.watch_socket(on_event)
 
