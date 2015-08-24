7c38040a32823ca3d3c80e62e3031bdf1ecc488d
ProgramFOX <programfox@hotmail.be>
2015-08-24 13:38:51 +0200

    Shorter load times for web client

--- webclient/client.js
+++ webclient/client.js
@@ -5,84 +5,82 @@ function escape(unescaped) {
                     .replace(/"/g, "&quot;");
 }
 
-function process(msg, payload) {
-    if (typeof (payload) === "undefined") payload = null;
+function process(msg, data) {
+    var isVerbose = msg[0] === "-";
+    msg = msg.substring(1);
+    if (isVerbose) data.verboseLog.unshift(msg);
+    else data.smallerLog.unshift(msg);
 
     var qlMatch = /^\[[0-9: -]+\] Queue length: (\d+)$/i.exec(msg);
     if (qlMatch !== null && qlMatch !== undefined) {
-        if (payload === null) {
-            document.getElementById("queueLengthCell").innerHTML = qlMatch[1];
-            return;
-        } else {
-            payload.queue = parseInt(qlMatch[1], 10);
-            return payload;
-        }
+        data.queue = parseInt(qlMatch[1], 10);
+        return data;
     }
 
     var quotaMatch = /^\[[0-9: -]+\] API quota: (\d+)$/i.exec(msg);
     if (quotaMatch !== null && quotaMatch !== undefined) {
-        if (payload === null) {
-            document.getElementById("quotaCell").innerHTML = quotaMatch[1];
-            return;
-        } else {
-            payload.quota = parseInt(quotaMatch[1], 10);
-            return payload;
-        }
+        data.quota = parseInt(quotaMatch[1], 10);
+        return data;
     }
 
-    var reportsTable = document.getElementById("reportsTable");
-    var newRow = reportsTable.insertRow(1);
-    var dateCell = newRow.insertCell(0);
-    var contentCell = newRow.insertCell(1);
-    dateCell.innerHTML = /^\[([0-9 :-]+)\]/.exec(msg)[1];
+    var report = { date: "", content: "" }
+    report.date = /^\[([0-9 :-]+)\]/.exec(msg)[1];
     msg = msg.replace(/^\[[0-9 :-]+\]/, "").trim();
-    contentCell.innerHTML = msg.replace(/\[([0-9]+)\]\(([^)]+)\)/, "<a href='$2'>$1</a>");
+    report.content = msg.replace(/\[([0-9]+)\]\(([^)]+)\)/, "<a href='$2'>$1</a>");
+    data.reports.push(report);
+    return data;
+}
 
-    if (payload !== null) return payload;
+var verboseOutput = "";
+var smallerOutput = "";
+var showVerbose = false;
+
+function updateDom(newQueueLength, newQuota, newReports, newVerboseLogData, newShorterLogData) {
+    if (newQueueLength !== -1) document.getElementById("queueLengthCell").innerHTML = newQueueLength.toString();
+    if (newQuota !== -1) document.getElementById("quotaCell").innerHTML = newQuota.toString();
+    if (newReports !== null && typeof (newReports) !== "undefined" && newReports.length > 0) {
+        var reportsTable = document.getElementById("reportsTable");
+        for (var i = 0; i < newReports.length; i++) {
+            var report = newReports[i];
+            var newRow = reportsTable.insertRow(1);
+            var dateCell = newRow.insertCell(0);
+            var contentCell = newRow.insertCell(1);
+            dateCell.innerHTML = report.date;
+            contentCell.innerHTML = report.content;
+        }
+    }
+    if (newVerboseLogData !== null && typeof (newVerboseLogData) !== "undefined" && newVerboseLogData.length > 0) {
+        verboseOutput = newVerboseLogData.concat([verboseOutput]).join("<br>");
+    }
+    if (newShorterLogData !== null && typeof (newShorterLogData) !== "undefined" && newShorterLogData.length > 0) {
+        smallerOutput = newShorterLogData.concat([smallerOutput]).join("<br>");
+    }
+    var logElem = document.getElementById("log");
+    if (showVerbose) logElem.innerHTML = verboseOutput;
+    else logElem.innerHTML = smallerOutput;
 }
 
 window.onload = function () {
-    var verboseOutput = "";
-    var smallerOutput = "";
-    var showVerbose = false;
-
+    document.getElementById("verboseChk").checked = false;
     var hostname = window.location.hostname;
     hostname = hostname == "" ? "localhost" : hostname;
     var isPayload = false;
-    var payload = { quota: -1, queue: -1 };
     var ws = new WebSocket("ws://" + hostname + ":4001/");
+    var data = { queue: -1, quota: -1, reports: [], verboseLog: [], smallerLog: [] };
     ws.onmessage = function (msg) {
-        var data = msg.data;
-        var verbose = data[0] == "-";
-        var message = data.substring(1);
-        if (data == "payload-start") {
+        var message = msg.data;
+        if (message == "payload-start") {
             isPayload = true;
             return;
-        } else if (data == "payload-end") {
+        } else if (message == "payload-end") {
             isPayload = false;
-            process("[0] Queue length: " + payload.queue.toString(), null);
-            process("[0] API quota: " + payload.quota.toString(), null);
+            updateDom(data.queue, data.quota, data.reports, data.verboseLog, data.smallerLog);
+            data = { queue: -1, quota: -1, reports: [], verboseLog: [], smallerLog: [] };
             return;
         }
-
-        if (isPayload) {
-            payload = process(message, payload);
-        } else {
-            process(message, null);
-        }
-
-        var logElem = document.getElementById("log");
-
-        verboseOutput = message + "<br>" + verboseOutput;
-        if (!verbose) {
-            smallerOutput = message + "<br>" + smallerOutput;
-        }
-
-        if (showVerbose) {
-            logElem.innerHTML = verboseOutput;
-        } else {
-            logElem.innerHTML = smallerOutput;
-        }
+ 
+        data = process(message, data);
+        if (!isPayload) updateDom(data.queue, data.quota, data.reports, data.verboseLog, data.smallerLog);
     };
 
     ws.onclose = function () {
